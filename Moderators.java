da574bbdd7a85d0a737339e53626fdd1cd678cec
Gadz1k <92876034+Gadz1k@users.noreply.github.com>
2021-12-03 11:02:44 +0100

    Delete Moderators.java

--- Moderators.java
+++
@@ -1,5 +0,0 @@
-public class Moderators extends Person {
-    public Moderators(String login, String email, String password, int id, int walletBalance) {
-        super(login, email, password, id, walletBalance);
-    }
-}
